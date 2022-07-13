7aad01f768054f027d53d5558ae83f2cb3de6c6f
Anthony Tran <anthonytran173@gmail.com>
2022-07-13 11:01:52 -0700

    Time: 135 ms (5.49%), Space: 14 MB (32.77%) - LeetHub

---
+++ 809-expressive-words/809-expressive-words.py
@@ -0,0 +1,38 @@
+class Solution:
+    def expressiveWords(self, s: str, words: List[str]) -> int:
+        def compress(word):
+            arr = []
+            count = 0
+            lastChar = word[0]
+            for char in word:
+                if lastChar == char:
+                    count += 1
+                else:
+                    arr.append({'char': lastChar, 'count': count})
+                    lastChar = char
+                    count = 1
+            arr.append({'char': lastChar, 'count': count})
+            return arr
+        
+        def test_word(word, arr_test):
+            arr_word = compress(word)
+            if len(arr_word) != len(arr_test):
+                return False
+            for i in range(0, len(arr_word)):
+                letter1 = arr_word[i]
+                letter2 = arr_test[i]
+                if letter1['char'] != letter2['char']:
+                    return False
+                elif letter1['count'] != letter2['count'] and letter1['count'] < 3:
+                    return False
+                elif letter1['count'] < letter2['count'] and letter1['count'] <= 3:
+                    return False
+            return True
+        
+        output = 0
+        
+        for word in words:
+            word_to_test = compress(word)
+            if test_word(s, word_to_test):
+                output += 1
+        return output
